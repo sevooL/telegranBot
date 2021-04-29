d46992d3b7de0c75d0edd8d8bffc9e874c1cc896
sevooL <83315944+sevooL@users.noreply.github.com>
2021-04-29 15:30:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ instagram : adimas_shadoet
 */
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1675749539:AAH46KNMRschhJDMuI5dclpp9FnB0znEaK4')
+const bot = new Telegraf('1786673675:AAEhJ0t1pQjGyEFEtzLD8v0B845xSpmVzxk')
 const axios = require('axios')
 
 /*********[ Apikey ]*********/
